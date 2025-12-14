7707b356efc9698f781180fd7895cdad12c1d566
carloslegnaro78 <carlos_legnaro@outlook.com>
2025-12-14 10:21:04 -0300

    Criando u m toolbar e configurando navegação entre páginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, MatButtonModule],
+  selector: 'app-root',  
+  imports: [RouterOutlet, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
