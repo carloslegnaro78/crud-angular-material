560d22f22c316a1c306743d8239e68eeecf74958
carlos.legnaro <carlos.legnaro@outlook.com>
2025-12-15 01:30:28 -0300

    Router Link e Material Icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule, MatIcon } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-root',  
-  imports: [RouterOutlet, MatToolbarModule],
+  selector: 'app-root',
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIcon],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
