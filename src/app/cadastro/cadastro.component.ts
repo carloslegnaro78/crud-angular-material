9183076e7b73101c1ef19da7179172dfd07b8b98
carlos.legnaro <carlos.legnaro@outlook.com>
2025-12-15 22:08:39 -0300

    Iniciando o Formulário de Clientes

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [FlexLayoutModule, MatCardModule, FormsModule, MatFormFieldModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
